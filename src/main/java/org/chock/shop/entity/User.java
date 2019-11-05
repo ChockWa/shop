7fab933740710f8718f17a3f2622e6054c4df21c
zhuohuahe <zhuohuahe@Ctrip.com>
2019-11-05 18:41:56 +0800

    提交sql

---
+++ src/main/java/org/chock/shop/entity/User.java
@@ -0,0 +1,21 @@
+package org.chock.shop.entity;
+
+/**
+ * @auther: zhuohuahe
+ * @date: 2019/11/5 17:03
+ * @description:
+ */
+public class User {
+
+//    uid varchar(36) not null,
+//    open_id varchar(64) not null,
+//    name varchar(16) null comment '用户名',
+//    password varchar(32) null comment '密码',
+//    salt varchar(8) null comment '盐',
+//    phone varchar(16) null comemnt '电话号码',
+//    email varchar(16) null comment '邮箱',
+//    status int(1) null comment '状态1-正常',
+//    gender int(1) null comment '性別1-男2-女',
+//    avatar_url varchar(64) comment '頭像',
+//    create_time datetime null comment '创建时间',
+}
